6ab5435a42131e454912a21054e5e49a0ff15863
GreatHorizon <52240672+GreatHorizon@users.noreply.github.com>
2020-10-05 08:52:15 +0300

    Changed main

--- lab2/Observer/WeatherStationPro/main.cpp
+++ lab2/Observer/WeatherStationPro/main.cpp
@@ -2,5 +2,15 @@
 
 int main()
 {
+	CWeatherData wd;
+	CWeatherData wd1;
+
+	CStatsDisplay obs;
+
+	wd.RegisterObserver(obs, 1);
+	wd.SetMeasurements(30, 2, 850, 15, 90);
+	wd.SetMeasurements(30, 2, 850, 15, 180);
+	wd.SetMeasurements(30, 2, 850, 15, 0);
+
 	return 0;
 }
