086a5b08d751e20dc1677ee4154aa8e952c6aa51
GreatHorizon <52240672+GreatHorizon@users.noreply.github.com>
2020-10-12 08:46:14 +0300

    Fixes

--- lab3/Streams/Streams/IStreamDecorator.h
+++ lab3/Streams/Streams/IStreamDecorator.h
@@ -6,28 +6,17 @@
 
 class IStreamDecorator : public IInputStream
 {
-
 public:
-	IStreamDecorator(std::unique_ptr<IInputStream>&& stream)
-		: m_stream(std::move(stream))
-	{
-	}
-
 	bool IsEOF() const
 	{
 		return m_stream->IsEOF();
 	}
 
-	uint8_t ReadByte() 
-	{
-		return m_stream->ReadByte();
-	}
-
-	std::streamsize ReadBlock(void* dstBuffer, std::streamsize size)
-	{
-		return m_stream->ReadBlock(dstBuffer, size);
-	}
-
 protected:
+	IStreamDecorator(std::unique_ptr<IInputStream>&& stream)
+		: m_stream(std::move(stream))
+	{
+	}
+
 	std::unique_ptr<IInputStream> m_stream;
 };
