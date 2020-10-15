dd5825d922c08c82abbb2a7b6c870abef1abce59
GreatHorizon <52240672+GreatHorizon@users.noreply.github.com>
2020-10-15 20:12:08 +0300

    Changed hint

--- lab4/Factory/Factory/main.cpp
+++ lab4/Factory/Factory/main.cpp
@@ -9,7 +9,8 @@ void ShowHint()
 		<< "rectangle <left top point X> <left top point Y> <width> <height> <color>\n"
 		<< "triangle <vertex1 X> <vertex1 Y> <vertex2 X> <vertex2 Y> <vertex3 X> <vertex3 Y> <color>\n"
 		<< "regularPolygon <center point X> <center poing Y> <radius> <color>\n"
-		<< "<color> could be: green, red, blue, yellow, black, pink\n\n";
+		<< "<color> could be: green, red, blue, yellow, black, pink"
+		<< "to start drawing your shapes enter eof\n\n";
 }
 
 int main()
